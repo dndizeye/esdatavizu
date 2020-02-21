f19231decf6f4d7375e1581a34669dd5430404f7
dndizeye <donald.ndizeye@gmail.com>
2020-02-21 01:38:49 +0100

    Update GithubApiOAuth2Config.java

--- src/main/java/fr/dndizeye/esdatavizu/config/GithubApiOAuth2Config.java
+++ src/main/java/fr/dndizeye/esdatavizu/config/GithubApiOAuth2Config.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GithubApiServiceOAuth2Config {
+public class GithubApiOAuth2Config {
 
 	@Value("${github.pta}")
 	private String oAuth2Token;
